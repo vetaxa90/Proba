bf49b917dea28daf0a02f7c850803255ef6c64e8
vetaxa90 <efimovich3@gmail.com>
2024-11-22 17:39:43 +0200

    Добавлена функция установки времени напоминания

--- Remmader.py
+++ Remmader.py
@@ -4,13 +4,29 @@ from tkinter import simpledialog as sd
 import  datetime
 import time
 import pygame
-from клик import window
+
+
+
+def set():
+    rem = sd.askstring("Время напоминания","введите время напоминания в формате чч:мм(в 24 часовом формате)")
+    if rem:
+        try:
+            hour =int(rem.split(":")[0])
+            minute=int(rem.split(":")[1])
+            now = datetime.datetime.now()
+            print(now)
+            dt= now.replace(hour=hour,minute =minute)
+            print(dt)
+            t= dt.timestamp()
+            print(t)
+        except Exception as e:
+            mb.showerror("Ошибка",f"Произошла ошибка {e}")
 
 window =Tk()
 window.title("Напоминание")
 label =Label(text="Установите напоминание")
 label.pack(pady=10)
 set_button =Button(text="Установить напоминание",command=set)
-set_button.pack
+set_button.pack()
 
 window.mainloop()
