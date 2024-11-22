d3a534047a9cabdfad5920fc07a04420fe120d36
vetaxa90 <efimovich3@gmail.com>
2024-11-22 19:34:51 +0200

    добавлена кнопка выключения музыки

--- Remmader.py
+++ Remmader.py
@@ -5,6 +5,7 @@ import  datetime
 import time
 import pygame
 t=0
+music= False
 
 
 def set():
@@ -34,18 +35,29 @@ def check():
 
 
 def play_snd():
+    global music
+    music = True
     pygame.mixer.init()
     pygame.mixer.music.load("reminder.mp3")
     pygame.mixer.music.play()
 
+def stop_music():
+    global music
+    if  music():
+         pygame.mixer.music.stop()
+         music=False
+    label.config(text = "Установить новое напоминание")
+
+
 
 window =Tk()
 window.title("Напоминание")
-label =Label(text="Установите напоминание",font=("Arial" 14))
+label =Label(text="Установите напоминание",font=("Arial",14))
 label.pack(pady=10)
 set_button =Button(text="Установить напоминание",command=set)
 set_button.pack()
-
+stop_button=Button(text="Остановить функцию",command =stop_music)
+stop_button.pack(pady=10)
 check()
 
 window.mainloop()
