7bf6eae1080ab062c8d7e2a9eac90f6778cefbc3
vetaxa90 <efimovich3@gmail.com>
2024-11-22 15:53:57 +0200

    Создаем главное окно

---
+++ Remmader.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import  messagebox as mb
+from tkinter import simpledialog as sd
+import  datetime
+import time
+import pygame
+from клик import window
+
+window =Tk()
+window.title("Напоминание")
+label =Label(text="Установите напоминание")
+label.pack(pady=10)
+set_button =Button(text="Установить напоминание",command=set)
+set_button.pack
+
+window.mainloop()
