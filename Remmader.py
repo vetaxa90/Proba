dc92bd7cdd4290229692d0650d12c1df285aa2b6
vetaxa90 <efimovich3@gmail.com>
2024-11-22 19:19:54 +0200

    Увеличен шрифт

--- Remmader.py
+++ Remmader.py
@@ -16,7 +16,7 @@ def set():
             minute=int(rem.split(":")[1])
             now = datetime.datetime.now()
             print(now)
-            dt= now.replace(hour=hour,minute =minute)
+            dt= now.replace(hour=hour,minute =minute,second=0)
             print(dt)
             t= dt.timestamp()
             print(t)
@@ -41,9 +41,11 @@ def play_snd():
 
 window =Tk()
 window.title("Напоминание")
-label =Label(text="Установите напоминание")
+label =Label(text="Установите напоминание",font=("Arial" 14))
 label.pack(pady=10)
 set_button =Button(text="Установить напоминание",command=set)
 set_button.pack()
+
+check()
 
 window.mainloop()
